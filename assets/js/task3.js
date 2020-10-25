5c328a672f6785827a85d0d1aa81e99f45424eb1
Yevhenii-Pierievierziev <evgeniuaevgeniu@gmail.com>
2020-10-25 09:49:44 +0200

    master

--- assets/js/task3.js
+++ assets/js/task3.js
@@ -71,30 +71,30 @@ const myArr = new MyArray('test', undefined, undefined, undefined, undefined, [[
 
 // 2) Написать функцию глубокого выравнивания для объекта. (flat для объектов).
 
-// const data = {
-// 	"dates": {
-// 		"expiry_date": "30 sep 2018",
-// 		"available": "30 sep 2017",
-// 	},
-// 	"price": {
-// 		"currency": "RM",
-// 		"min": 1500,
-// 		"max": 2000
-// 	}
-// };
-// const flatten = (obj, prefix = '', res = {}) =>
-// 	Object.entries(obj).reduce((r, [key, val]) => {
-// 		const k = `${prefix}${key}`
-// 		if (typeof val === 'object') {
-// 			flatten(val, `${k}.`, r)
-// 		} else {
-// 			res[k] = val;
-// 		}
-// 		return r;
-// 	}, res);
+const data = {
+	"dates": {
+		"expiry_date": "30 sep 2018",
+		"available": "30 sep 2017",
+	},
+	"price": {
+		"currency": "RM",
+		"min": 1500,
+		"max": 2000
+	}
+};
+const flatten = (obj, prefix = '', res = {}) =>
+	Object.entries(obj).reduce((r, [key, val]) => {
+		const k = `${prefix}${key}`
+		if (typeof val === 'object') {
+			flatten(val, `${k}.`, r)
+		} else {
+			res[k] = val;
+		}
+		return r;
+	}, res);
 
 
-// console.log(flatten(data))
+console.log(flatten(data))
 
 //=================================================================================
 
@@ -122,23 +122,23 @@ logPar(5);
 // 4) Создать объект user'а. У объекта есть имя, дата рождения(Использовать Date в js), количество отработанных часов за месяц и ставка за час.
 //     Реализовать геттеры (get age) на получение возраста и заработной платы.
 
-// const user = {
-// 	name: 'Vasya',
-// 	birthDay: new Date(1992, 6, 1),
-// 	working: 280,
-// 	payForHour: 9,
+const user = {
+	name: 'Vasya',
+	birthDay: new Date(1992, 6, 1),
+	working: 280,
+	payForHour: 9,
 
-// 	get age() {
-// 		let todayYear = new Date().getFullYear();
-// 		return todayYear - this.birthDay.getFullYear();
-// 	},
-// 	get sallary() {
-// 		return this.payForHour * this.working;
-// 	}
-// };
+	get age() {
+		let todayYear = new Date().getFullYear();
+		return todayYear - this.birthDay.getFullYear();
+	},
+	get sallary() {
+		return this.payForHour * this.working;
+	}
+};
 
-// alert(user.age);
-// alert(user.sallary);
+alert(user.age);
+alert(user.sallary);
 
 //=================================================================================
 
@@ -146,33 +146,33 @@ logPar(5);
 // Гласными являются «a», «e», «i», «o», «u».
 // строки - итерируемые.
 
-// function numOfVowels(string) {
-// 	let count = 0;
+function numOfVowels(string) {
+	let count = 0;
 
-// 	for (let i = 0; i < string.length; i++) {
-// 		if (string[i].toLowerCase() === 'a' || string[i].toLowerCase() === 'e' || string[i].toLowerCase() === 'i' || string[i].toLowerCase() === 'o' || string[i].toLowerCase() === 'u') {
-// 			count++;
-// 		}
-// 	}
-// 	return count;
-// }
+	for (let i = 0; i < string.length; i++) {
+		if (string[i].toLowerCase() === 'a' || string[i].toLowerCase() === 'e' || string[i].toLowerCase() === 'i' || string[i].toLowerCase() === 'o' || string[i].toLowerCase() === 'u') {
+			count++;
+		}
+	}
+	return count;
+}
 
-// console.log(numOfVowels('Yellow Submarine'));
+console.log(numOfVowels('Yellow Submarine'));
 
-// const words = s => s.split("").filter(c => "aeiou".includes(c)).length;
+const words = s => s.split("").filter(c => "aeiou".includes(c)).length;
 
-// console.log(words("Yellow Submarine")); // 6
+console.log(words("Yellow Submarine")); // 6
 
-// const findVolews = str => {
-// 	let count = 0;
-// 	const vowels = ['a', 'e', 'i', 'o', 'u'];
-// 	for (let char of str.toLowerCase()) {
-// 		if (vowels.includes(char)) {
-// 			count++
-// 		}
-// 	}
-// 	return count;
-// }
+const findVolews = str => {
+	let count = 0;
+	const vowels = ['a', 'e', 'i', 'o', 'u'];
+	for (let char of str.toLowerCase()) {
+		if (vowels.includes(char)) {
+			count++
+		}
+	}
+	return count;
+}
 
 //=================================================================================
 
@@ -181,75 +181,75 @@ logPar(5);
 // вывод fizz вместо чисел, кратных 3;
 // вывод buzz вместо чисел, кратных 5;
 
-// const fizzBuzz = num => {
-// 	for (let i = 1; i <= num; i++) {
-// 		if (i % 3 === 0 && i % 5 === 0) {
-// 			console.log('fizzbuzz');
-// 		} else if (i % 3 === 0) {
-// 			console.log('fizz');
-// 		} else if (i % 5 === 0) {
-// 			console.log('buzz');
-// 		} else {
-// 			console.log(i);
-// 		}
-// 	}
-// }
+const fizzBuzz = num => {
+	for (let i = 1; i <= num; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log('fizzbuzz');
+		} else if (i % 3 === 0) {
+			console.log('fizz');
+		} else if (i % 5 === 0) {
+			console.log('buzz');
+		} else {
+			console.log(i);
+		}
+	}
+}
 
 //=================================================================================
 
-// 3) Создайте функцию avg() , которая будет находить среднее значение по всем своим аргументам.
+3) Создайте функцию avg(), которая будет находить среднее значение по всем своим аргументам.
 
-// function avg(...nums) {
-// 	return nums.reduce((a, b) => (a + b)) / nums.length;
-// }
+function avg(...nums) {
+	return nums.reduce((a, b) => (a + b)) / nums.length;
+}
 
-// console.log(avg(2, 4, 7, 9, 16));
+console.log(avg(2, 4, 7, 9, 16));
 
 //=================================================================================
 
 // 4) Напишите функцию addNum(n), которая вернёт другую функцию. Возвращенная функция должна складывать получаемый аргумент с аргументом n возвращающей функции.
 // замыкание.
 
-// function addNum(initialValue = 0) {
+function addNum(initialValue = 0) {
 
-// 	let accumulate = initialValue;
+	let accumulate = initialValue;
 
-// 	return function sumNumber(number) {
-// 		return (accumulate = accumulate + number);
-// 	}
-// };
+	return function sumNumber(number) {
+		return (accumulate = accumulate + number);
+	}
+};
 
 //=================================================================================
 
 // 5) Напишите функцию operation(num1, num2, fn), в которой num1 и num2 — числовые переменные, а fn — функция, которая берет два аргумента и выполняет математическую операцию над ними
 
-// function operation(num1, num2, fn) {
-// 	return fn(num1, num2);
-// }
+function operation(num1, num2, fn) {
+	return fn(num1, num2);
+}
 
-// const sumNumbers = (number1, number2) => {
-// 	return number1 + number2;
-// };
+const sumNumbers = (number1, number2) => {
+	return number1 + number2;
+};
 
-// console.log(operation(10, 15, sumNumbers));
+console.log(operation(10, 15, sumNumbers));
 
 //=================================================================================
 
 // 6) Создать объект obj, с методами method1(),method2() и method3(). В методе method3() должна возвращаться строка «метод3». Сделайте так, чтобы было возможно выполнение кода obj.method1().method2().method3()
 
-// const obj = {
-// 	method1() {
-// 		return this;
-// 	},
-// 	method2() {
-// 		return this;
-// 	},
-// 	method3() {
-// 		return "method 3";
-// 	},
-// };
+const obj = {
+	method1() {
+		return this;
+	},
+	method2() {
+		return this;
+	},
+	method3() {
+		return "method 3";
+	},
+};
 
-// obj.method1().method2().method3();
+obj.method1().method2().method3();
 
 //=================================================================================
 
